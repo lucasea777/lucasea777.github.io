1b9b9be76b726fa6dc7d4724b8af39f9f7ad363f
Lucas Alonso <lucas.alonso@winclap.com>
2023-01-03 18:56:55 -0300

    counter details

---
+++ lym/_next/static/bsS4Bk6NSPxBXTe6EKTab/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/82-74ff18177ad2811a.js","static/css/23306995ad67020b.css","static/chunks/pages/index-c8203fb7e4a9fdce.js"],"/_error":["static/chunks/pages/_error-82a61e6508dae5a8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
