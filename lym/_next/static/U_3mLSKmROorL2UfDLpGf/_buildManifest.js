473c4455f1d4f4685d0af65580675a89dfd3a70f
Lucas Alonso <lucas.alonso@winclap.com>
2023-01-03 15:27:54 -0300

    copied out from nextjs export as lym (and possibly renamed en.html to index.html)

---
+++ lym/_next/static/U_3mLSKmROorL2UfDLpGf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/82-74ff18177ad2811a.js","static/css/737522b1d195ad26.css","static/chunks/pages/index-a193073329e6b5a9.js"],"/_error":["static/chunks/pages/_error-82a61e6508dae5a8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
