55de8d063e451f422ac2146cb8e940266aa86247
Lucas Alonso <lucas.alonso@winclap.com>
2023-01-04 23:15:20 -0300

    even smaller devices

--- lym/_next/static/EWGL8w71_jFOlP7aUp0_N/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/82-74ff18177ad2811a.js","static/css/23306995ad67020b.css","static/chunks/pages/index-72df7a361b888697.js"],"/_error":["static/chunks/pages/_error-82a61e6508dae5a8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
